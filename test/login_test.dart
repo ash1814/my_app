86e0aa7d2f426206906cc57b298807b8b9542c56
AShiqur Rahman <ashiqdt.ar@gmail.com>
2021-06-20 10:24:08 +0600

    done

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,7 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_app/main.dart';
+
+import '';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('login ...', (tester) async {
-    // TODO: Implement test
+  testWidgets('login checking Submit button', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    var button = find.text("Submit");
+    expect(button, findsOneWidget);
+
+
   });
+
+  testWidgets('login checking user field', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    var textFormfield = find.textContaining("Enter a number");
+    expect(textFormfield, findsOneWidget);
+
+});
+  // testWidgets('login checking password field', (WidgetTester tester) async {
+  //   await tester.pumpWidget(MyApp());
+  //
+  //     var pass = find.textContaining("Enter your password");
+  //     expect(pass, findsOneWidget);
+  //
+  //
+  //   });
+
+
 }
